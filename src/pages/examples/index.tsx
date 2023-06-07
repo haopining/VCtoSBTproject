3405b4efed613e1e907a66b588a76bb8c79d7ff6
haopin <haopining@gmail.com>
2023-06-07 10:29:19 +0800

    modify and delete some unnecessary file

--- src/pages/examples/index.tsx
+++ src/pages/examples/index.tsx
@@ -3,20 +3,14 @@ import { HeadingComponent } from 'components/layout/HeadingComponent'
 import { CardList } from 'components/layout/CardList'
 import { Code, Text } from '@chakra-ui/react'
 
-import AuthIcon from 'assets/icons/auth.png'
 import NFTIcon from 'assets/icons/nft.png'
 
 
 export const ExampleItems = [
+ 
   {
-    title: 'Sign-in with Ethereum',
-    description: 'Sign-in with Ethereum is a new form of authentication that enables users to control their identity with their Ethereum account.',
-    image: AuthIcon.src,
-    url: '/examples/siwe',
-  },
-  {
-    title: 'Mint NFT',
-    description: 'A Non-Fungible Token (NFT) is used to identify something or someone in a unique way. Use this ERC721 example to mint your own NFT.',
+    title: 'Mint SBT',
+    description: 'Use this example to mint your own SBT.',
     image: NFTIcon.src,
     url: '/examples/mint-nft',
   }
